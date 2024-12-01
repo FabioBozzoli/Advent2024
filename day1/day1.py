641eb586c6791d3d992efaac1ee66b2ff4d9ed81
FabioBozzoli <119732599+FabioBozzoli@users.noreply.github.com>
2024-12-01 19:34:05 +0100

    day1.py

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("input.txt", sep = " ", header = None)
+left, right = df[0], df[3]
+left = np.array(left.sort_values())
+right = np.array(right.sort_values())
+
+print(abs(left-right).sum())
+
+somma = 0
+for i in left:
+    count = (i==right).sum()
+    somma += i*count
+print(somma)
