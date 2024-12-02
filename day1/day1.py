f6b1062358c9ebdb49a177f4a54b05795c6be078
FabioBozzoli <119732599+FabioBozzoli@users.noreply.github.com>
2024-12-02 21:52:09 +0100

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -7,9 +7,13 @@ left = np.array(left.sort_values())
 right = np.array(right.sort_values())
 
 print(abs(left-right).sum())
-
+'''
 somma = 0
 for i in left:
     count = (i==right).sum()
     somma += i*count
+'''
+
+somma = ((left.reshape(-1,1) == right.reshape(1,-1)).sum(axis = 1) * left).sum()
+
 print(somma)
